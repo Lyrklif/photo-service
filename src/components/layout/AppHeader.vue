9945f94da4e9a64a8c186cfd0c3f3d8452c70d24
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-15 22:17:08 +0400

    feat: change favorite btn tag

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -6,13 +6,45 @@ import { useRoute } from "vue-router";
 import { PAGE_NAMES } from "../../constants/router";
 const route = useRoute();
 
-const logo = computed(() => {
-  const isStatic = route.name === PAGE_NAMES.main;
+const isMainPage = computed(() => {
+  return route.name === PAGE_NAMES.main;
+});
 
+const logo = computed(() => {
   return {
-    tag: isStatic ? "div" : "router-link",
-    to: isStatic ? null : { name: PAGE_NAMES.main },
+    tag: isMainPage.value ? "div" : "router-link",
+    to: isMainPage.value ? null : { name: PAGE_NAMES.main },
   };
+});
+
+const isShowSearch = computed(() => {
+  return !isMainPage.value;
+});
+
+const favorite = computed(() => {
+  switch (route.name) {
+    case PAGE_NAMES.favorites:
+      return {
+        disabled: true,
+        customTag: "div",
+        to: undefined,
+        button: false,
+      };
+    case PAGE_NAMES.photo:
+      return {
+        disabled: false,
+        customTag: undefined,
+        to: undefined,
+        button: true,
+      };
+    default:
+      return {
+        disabled: false,
+        customTag: undefined,
+        to: { name: PAGE_NAMES.favorites },
+        button: false,
+      };
+  }
 });
 </script>
 
@@ -23,11 +55,16 @@ const logo = computed(() => {
         <AppIcon name="logo" class="logo-icon" />
       </component>
 
-      <AppButton>
+      <AppButton v-if="isShowSearch" :to="{ name: PAGE_NAMES.main }">
         <AppIcon name="search" class="icon" />
         <span>Поиск</span>
       </AppButton>
-      <AppButton customTag="div" disabled>
+
+      <AppButton
+        :customTag="favorite.customTag"
+        :disabled="favorite.disabled"
+        :to="favorite.to"
+      >
         <AppIcon name="heart" class="icon" />
         <span>Избранное</span>
       </AppButton>
