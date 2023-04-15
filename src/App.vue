3ea50613f9876ef7dc4e422459332677529ca0d7
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-15 20:01:30 +0400

    refactor: update components structure

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script lang="ts" setup>
+import AppHeader from "./components/layout/AppHeader.vue";
+</script>
+
 <template>
+  <AppHeader />
   <router-view />
 </template>
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header>App Header</header>
+</template>
