880b87bb7a58a09b0641351f273cc4c822388450
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-16 17:35:08 +0400

    feat: add SearchForm

---
+++ src/components/search/SearchForm.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import AppInput from "../ui/AppInput.vue";
+import IconButton from "../ui/IconButton.vue";
+
+const search = ref("");
+const emits = defineEmits(["submit"]);
+
+const setValue = (value: string) => {
+  search.value = value;
+};
+
+const submitHandler = () => {
+  emits("submit", search.value);
+};
+</script>
+
+<template>
+  <form class="form" @submit.prevent="submitHandler">
+    <AppInput @input="setValue" />
+    <IconButton icon="search" class="submit" type="submit" />
+  </form>
+</template>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/colors";
+@import "../../assets/styles/breakpoints";
+
+.form {
+  display: flex;
+  align-items: center;
+}
+
+.submit {
+  background-color: $white;
+  height: 70px;
+  width: 70px;
+  color: $dark;
+}
+</style>
