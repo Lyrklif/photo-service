6a4e9f7fded96c095963d04cfe895e86c414ff8a
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-16 15:03:08 +0400

    feat: background image

--- src/components/search/SearchPanel.vue
+++ src/components/search/SearchPanel.vue
@@ -1,10 +1,16 @@
 <script lang="ts" setup>
-//
+import AppImage from "../ui/AppImage.vue";
 </script>
 
 <template>
   <div class="search-panel">
     <div class="container">Search</div>
+
+    <AppImage
+      src="background.png"
+      alt="'Звездная ночь' Винсента Ван Гога"
+      class="bg-image"
+    />
   </div>
 </template>
 
@@ -16,11 +22,23 @@
   padding-top: 80px;
   padding-bottom: 80px;
   background-color: $dark-background;
+  position: relative;
 
   @include md-up() {
     padding-top: 92px;
     padding-bottom: 92px;
     border-bottom: 16px solid $grey;
   }
+}
+
+.bg-image {
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  pointer-events: none;
 }
 </style>
