636204bcc2b6375f3c5699039cffffb1f341b297
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-17 22:53:51 +0400

    fix: update bg image path

--- src/components/search/SearchBackground.vue
+++ src/components/search/SearchBackground.vue
@@ -3,8 +3,8 @@ import AppPicture from "../ui/AppPicture.vue";
 
 const source = [
   {
-    srcset: `src/assets/images/background-desktop@2x.png 2x,
-      src/assets/images/background-desktop.png`,
+    srcset: `background-desktop@2x.png 2x,
+      background-desktop.png`,
     media: "(min-width: 768px)",
   },
 ];
@@ -14,8 +14,8 @@ const source = [
   <AppPicture
     class="bg-image"
     :source="source"
-    srcset="src/assets/images/background@2x.png 2x"
-    src="src/assets/images/background.png"
+    srcset="background@2x.png 2x"
+    src="background.png"
     alt="'Звездная ночь' Винсента Ван Гога"
   />
 </template>
