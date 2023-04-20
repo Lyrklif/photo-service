43712a285c0a9ced11745a783d29f5e56940ff1d
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-21 00:52:42 +0400

    feat: (process) refresh token

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import type { AxiosInstance } from "axios";
 import { useAuthStore } from "../stores/auth";
+import { getAuthCodeUrl } from "./auth";
+import { INVALID_ACCESS_TOKEN } from "../common/constants/errors";
 
 const instance: AxiosInstance = axios.create({
   baseURL: "https://api.unsplash.com/",
@@ -8,17 +10,27 @@ const instance: AxiosInstance = axios.create({
     // token for public api
     Authorization: `Client-ID ${import.meta.env.VITE_ACCESS_KEY}`,
   },
-  transformRequest: [
-    function (data, headers) {
-      const store = useAuthStore();
-
-      if (store.accessToken) {
-        headers.Authorization = `Bearer ${store.accessToken}`;
-      }
-
-      return data;
-    },
-  ],
 });
+
+instance.interceptors.request.use((config) => {
+  const store = useAuthStore();
+  if (store.accessToken) {
+    config.headers.Authorization = `Bearer ${store.accessToken}`;
+  }
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async function (error) {
+    if (error.response.status === INVALID_ACCESS_TOKEN) {
+      // Set a new Access Token
+      window.location.href = getAuthCodeUrl();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
